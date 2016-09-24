92eecf4fce048a58ba9336eadae50261749005cc
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-09-24 21:31:30 +0100

    Transpile with babel es2015-script preset

--- launchpad.js
+++ launchpad.js
@@ -10,7 +10,7 @@ var midi = require('midi');
  * Launchpad
  * Represents the launchpad as a whole
  */
-var Launchpad = function(port, initAnimation) {
+var Launchpad = function Launchpad(port, initAnimation) {
     if (initAnimation === undefined) initAnimation = true;
     var name = 0;
     var row = 0;
@@ -27,31 +27,31 @@ var Launchpad = function(port, initAnimation) {
     var that = this;
 
     this.specials = {
-        0:{ 8: ["row","vol"] },
-        1:{ 8: ["row","pan"] },
-        2:{ 8: ["row","snd A"] },
-        3:{ 8: ["row","snd B"] },
-        4:{ 8: ["row","stop"] },
-        5:{ 8: ["row","trk on"] },
-        6:{ 8: ["row","solo"] },
-        7:{ 8: ["row","arm"] },
-        8:{
-            0:["up","page"],
-            1:["down","page"],
-            2:["left","page"],
-            3:["right","page"],
-            4:["session","inst"],
-            5:["user 1","fx"],
-            6:["user 2","user"],
-            7:["mixer","mixer"],
-            8:null
+        0: { 8: ["row", "vol"] },
+        1: { 8: ["row", "pan"] },
+        2: { 8: ["row", "snd A"] },
+        3: { 8: ["row", "snd B"] },
+        4: { 8: ["row", "stop"] },
+        5: { 8: ["row", "trk on"] },
+        6: { 8: ["row", "solo"] },
+        7: { 8: ["row", "arm"] },
+        8: {
+            0: ["up", "page"],
+            1: ["down", "page"],
+            2: ["left", "page"],
+            3: ["right", "page"],
+            4: ["session", "inst"],
+            5: ["user 1", "fx"],
+            6: ["user 2", "user"],
+            7: ["mixer", "mixer"],
+            8: null
         }
     };
 
-    this.createButtons = function() {
+    this.createButtons = function () {
         // Initialize all of the buttons
-        for(var y = 0; y < 9; y++) {
-            for(var x = 0; x < 9; x++) {
+        for (var y = 0; y < 9; y++) {
+            for (var x = 0; x < 9; x++) {
                 if (that._grid[x] === undefined) that._grid[x] = [];
                 that._grid[x][y] = new Button(that, x, y);
             }
@@ -61,9 +61,9 @@ var Launchpad = function(port, initAnimation) {
     /*
      * Gets a button object from this._grid
      */
-    this.getButton = function(x, y) {
+    this.getButton = function (x, y) {
         if (y === undefined) {
-            y = Math.floor(x/16);
+            y = Math.floor(x / 16);
             x = x % 16;
         }
 
@@ -71,8 +71,7 @@ var Launchpad = function(port, initAnimation) {
             return undefined;
         }
 
-        if(y !== undefined)
-            return this._grid[x][y];
+        if (y !== undefined) return this._grid[x][y];
 
         var map = mapButtonToLaunchpad(x);
         return this._grid[map[0]][map[1]];
@@ -81,10 +80,10 @@ var Launchpad = function(port, initAnimation) {
     /*
      * Turns all LEDs off
      */
-    this.allDark = function() {
+    this.allDark = function () {
         // Reset the state on all buttons
-        for(var x = 0; x < 9; x++) {
-            for(var y = 0; y < 9; y++) {
+        for (var x = 0; x < 9; x++) {
+            for (var y = 0; y < 9; y++) {
                 this._grid[x][y].dark();
             }
         }
@@ -95,10 +94,10 @@ var Launchpad = function(port, initAnimation) {
     /*
      * Turns all LEDs on
      */
-    this.allLight = function(color) {
+    this.allLight = function (color) {
         // Reset the state on all buttons
-        for(var x = 0; x < 9; x++) {
-            for(var y = 0; y < 9; y++) {
+        for (var x = 0; x < 9; x++) {
+            for (var y = 0; y < 9; y++) {
                 this._grid[x][y].light(color);
             }
         }
@@ -107,72 +106,66 @@ var Launchpad = function(port, initAnimation) {
     /*
      * Event handler for button press
      */
-    this.receiveMessage = function(deltaTime, msg) {
+    this.receiveMessage = function (deltaTime, msg) {
         // We have to do something special for the top buttons
         var button = that.getButton(msg[1]);
-        if(msg[0] == "176")
-            button = that.getButton(parseInt(msg[1],10) % 8, 8);
-
+        if (msg[0] == "176") button = that.getButton(parseInt(msg[1], 10) % 8, 8);
 
         // On or off?
-        var state = (parseInt( msg[ 2 ], 10 ) == 127);
+        var state = parseInt(msg[2], 10) == 127;
 
         // Emit an event
-        if(state) {
-            button.trigger("press",deltaTime);
+        if (state) {
+            button.trigger("press", deltaTime);
             button.trigger("state_change");
             this.trigger("press", button);
             if (button.special !== false) this.trigger("special_press", button);
-
         } else {
-            button.trigger("release",deltaTime);
+            button.trigger("release", deltaTime);
             button.trigger("state_change");
             that.trigger("release", button);
             if (button.special !== false) that.trigger("special_release", button);
         }
     };
 
-    this.randomColor = function() {
+    this.randomColor = function () {
         var options = [3, 48, 18, 49];
         var rand = Math.floor(Math.random() * options.length);
         return options[rand];
     };
 
-    this.initialize = function() {
+    this.initialize = function () {
         if (!initAnimation) {
             // console.log("hi");
             // that.clear();
-            setTimeout(function() {
+            setTimeout(function () {
                 that.trigger("ready", that);
                 that.ready = true;
-            },0);
+            }, 0);
             return;
         }
 
-        var colors = [
-            3, 48, 18, 49, 0
-        ];
+        var colors = [3, 48, 18, 49, 0];
 
-        for (var j = 0; j < colors.length;j++) {
+        for (var j = 0; j < colors.length; j++) {
             var i = colors[j];
-            (function(i){
-                setTimeout(function() {
-                    for(var x = 0; x < 9; x++) {
-                        for(var y = 0; y < 9; y++) {
-                            (function(x,y){
-                                var t = setTimeout(function() {
+            (function (i) {
+                setTimeout(function () {
+                    for (var x = 0; x < 9; x++) {
+                        for (var y = 0; y < 9; y++) {
+                            (function (x, y) {
+                                var t = setTimeout(function () {
                                     try {
                                         that._grid[x][y].light(i);
                                         if (i === 0 && x === 8 && y === 8) {
                                             that.trigger("ready", that);
                                             that.ready = true;
                                         }
+                                    } catch (e) {
+                                        console.log(x + " " + y);
                                     }
-                                    catch (e) {
-                                        console.log(x+" "+y);
-                                    }
-                                },(x+y)*100);
-                            })(x,y);
+                                }, (x + y) * 100);
+                            })(x, y);
                         }
                     }
                 }, 500 * j);
@@ -180,10 +173,9 @@ var Launchpad = function(port, initAnimation) {
         }
     };
 
-
     this.ready = false;
-    this.init = function() {
-            // Set up a new output.
+    this.init = function () {
+        // Set up a new output.
         this.output = new midi.output();
         // Set up a new input.
         this.input = new midi.input();
@@ -195,42 +187,40 @@ var Launchpad = function(port, initAnimation) {
             this.output.openPort(port);
             this.input.openPort(port);
             console.log('Running launchpad on port ' + port + this.output.getPortName(port));
-
         } else {
             // Detect Launchpad by checking the port names
-            let openLaunchpadPort = function(io, name) {
-                return (new Array(io.getPortCount())).fill(0)
-                    .map((el,ix) => ( { name: io.getPortName(ix), port: ix } ))
-                    .filter(desc => desc.name.indexOf('Launchpad') >= 0)
-                    .some(desc => {
-                        console.log(`Opening ${name} port ${desc.port}: ${desc.name}`);
-                        io.openPort(desc.port);
-                        return true;
-                    });
+            var openLaunchpadPort = function openLaunchpadPort(io, name) {
+                return new Array(io.getPortCount()).fill(0).map(function (el, ix) {
+                    return { name: io.getPortName(ix), port: ix };
+                }).filter(function (desc) {
+                    return desc.name.indexOf('Launchpad') >= 0;
+                }).some(function (desc) {
+                    console.log("Opening " + name + " port " + desc.port + ": " + desc.name);
+                    io.openPort(desc.port);
+                    return true;
+                });
             };
 
-            let openedOutput = openLaunchpadPort(this.output, 'output'),
+            var openedOutput = openLaunchpadPort(this.output, 'output'),
                 openedInput = openLaunchpadPort(this.input, 'input');
 
             if (!openedInput) throw new Error('Could not detect Launchpad on input ports.');
-            if (!openedOutput) throw new Error('Could not detect Launchpad on output ports.')
+            if (!openedOutput) throw new Error('Could not detect Launchpad on output ports.');
         }
 
-
         // Configure a callback.
-        this.input.on('message', function(deltaTime, message) {
+        this.input.on('message', function (deltaTime, message) {
             that.receiveMessage(deltaTime, message);
         });
 
         this.initialize();
     };
 
-    console.log('Setting up Launchpad on ' + (port ? `port ${port}` : 'auto-detected port'));
+    console.log('Setting up Launchpad on ' + (port ? "port " + port : 'auto-detected port'));
     this.init();
-
 };
 
-Launchpad.prototype.renderByte = function(x, y, color, byte) {
+Launchpad.prototype.renderByte = function (x, y, color, byte) {
 
     byte = byte.toLowerCase();
     switch (byte) {
@@ -255,13 +245,13 @@ Launchpad.prototype.renderByte = function(x, y, color, byte) {
     this._grid[y][x].light(color);
 };
 
-Launchpad.prototype.renderColors = function(colors) {
+Launchpad.prototype.renderColors = function (colors) {
     if (colors === undefined) return;
-        for (var x = 0; x < colors.length; x++) {
+    for (var x = 0; x < colors.length; x++) {
         var colorColumn = colors[x];
         for (var y = 0; y < colorColumn.length; y++) {
             if (!this._grid[y][x]) {
-                console.log("Button not found: x:"+x+", y:"+y);
+                console.log("Button not found: x:" + x + ", y:" + y);
                 return;
             }
             this._grid[y][x].light(colorColumn[y]);
@@ -269,72 +259,71 @@ Launchpad.prototype.renderColors = function(colors) {
     }
 };
 
-Launchpad.prototype.renderBytes = function(bytes, color) {
+Launchpad.prototype.renderBytes = function (bytes, color) {
     if (bytes === undefined) return;
-        for (var x = 0; x < bytes.length; x++) {
+    for (var x = 0; x < bytes.length; x++) {
         var byt = bytes[x];
         for (var y = 0; y < byt.length; y++) {
             if (!this._grid[y][x]) {
-                console.log("Button not found: x:"+x+", y:"+y);
+                console.log("Button not found: x:" + x + ", y:" + y);
                 return;
             }
-            this.renderByte(x, y, color,  byt[y]);
+            this.renderByte(x, y, color, byt[y]);
         }
     }
 };
 
-Launchpad.prototype.displayCharacter = function(letter, color) {
-  var bytes = LawrenceSans(letter);
-  this.renderBytes(bytes, color);
+Launchpad.prototype.displayCharacter = function (letter, color) {
+    var bytes = LawrenceSans(letter);
+    this.renderBytes(bytes, color);
 };
 
-Launchpad.prototype.displayString = function(str, delay, callback, color) {
-  if (delay === undefined) delay = 500;
-  var that = this;
-  for (var j = 0; j < str.length; j++) {
-    (function(j){
-      setTimeout(function() {
-        that.displayCharacter(str[j], color);
-        if (j+1 === str.length && callback !== undefined) setTimeout(callback, delay);
-      }, j*delay);
-    })(j);
-  }
+Launchpad.prototype.displayString = function (str, delay, callback, color) {
+    if (delay === undefined) delay = 500;
+    var that = this;
+    for (var j = 0; j < str.length; j++) {
+        (function (j) {
+            setTimeout(function () {
+                that.displayCharacter(str[j], color);
+                if (j + 1 === str.length && callback !== undefined) setTimeout(callback, delay);
+            }, j * delay);
+        })(j);
+    }
 };
 
-Launchpad.prototype.scrollString = function(str,delay, color, onFinished) {
+Launchpad.prototype.scrollString = function (str, delay, color, onFinished) {
     var bytes = [];
-    for(var i = 0; i < str.length; i++) {
+    for (var i = 0; i < str.length; i++) {
         bytes.push(LawrenceSans(str[i]));
-        bytes.push(["0","0","0","0","0","0","0","0"]);
+        bytes.push(["0", "0", "0", "0", "0", "0", "0", "0"]);
     }
     this.scrollBytes(bytes, delay, color, onFinished);
 };
 
-Launchpad.prototype.colorize = function(bytes, colorStr, nullColorStr) {
+Launchpad.prototype.colorize = function (bytes, colorStr, nullColorStr) {
     for (var i = 0; i < bytes.length; i++) {
         var str = "";
         for (var j = 0; j < bytes[i].length; j++) {
-            if (bytes[i][j] === "1") str += colorStr;
-            else  str += nullColorStr;
+            if (bytes[i][j] === "1") str += colorStr;else str += nullColorStr;
         }
         bytes[i] = str;
     }
     return bytes;
 };
 
-Launchpad.prototype.clearScroll = function() {
+Launchpad.prototype.clearScroll = function () {
     clearInterval(this.scrollInterval);
 };
 
-Launchpad.prototype.scrollBytes = function(bytes, delay, color, onFinished) {
+Launchpad.prototype.scrollBytes = function (bytes, delay, color, onFinished) {
     var perScreen = 8;
     var charPos = 0;
     var overallBytes = [];
     var interval = null;
 
-    for (var i= 0; i < bytes[0].length; i++) {
+    for (var i = 0; i < bytes[0].length; i++) {
         var toAdd = "";
-        for (var j = 0; j < bytes.length;j++) {
+        for (var j = 0; j < bytes.length; j++) {
             toAdd += bytes[j][i];
         }
         overallBytes.push(toAdd);
@@ -345,15 +334,13 @@ Launchpad.prototype.scrollBytes = function(bytes, delay, color, onFinished) {
 
     if (delay === undefined) delay = 200;
 
-    var visibleSet = function() {
+    var visibleSet = function visibleSet() {
         var toReturn = [];
         for (var i = 0; i < overallBytes.length; i++) {
             toReturn[i] = new Array(perScreen);
             var str = "";
-            for (var j = charPos; j < charPos+perScreen; j++) {
-                if (overallBytes[i][j] !== undefined)
-                    str += overallBytes[i][j];
-                else {
+            for (var j = charPos; j < charPos + perScreen; j++) {
+                if (overallBytes[i][j] !== undefined) str += overallBytes[i][j];else {
                     clearInterval(interval);
                     return;
                 }
@@ -364,41 +351,39 @@ Launchpad.prototype.scrollBytes = function(bytes, delay, color, onFinished) {
         return toReturn;
     };
 
-    interval = setInterval(function() {
+    interval = setInterval(function () {
         var set = visibleSet();
-        if (set === undefined && typeof onFinished == "function") onFinished();
-        else
-            that.renderBytes(set, color);
+        if (set === undefined && typeof onFinished == "function") onFinished();else that.renderBytes(set, color);
     }, delay);
-
 };
 
 exports.colors = Launchpad.prototype.colors = {
     off: 0,
     red: {
         low: 1,
-        medium:2,
-        high:3
+        medium: 2,
+        high: 3
     },
     yellow: {
         low: 17,
-        medium:34,
-        high:54
+        medium: 34,
+        high: 54
     },
     orange: {
         low: 45,
-        medium:46,
-        high:23
+        medium: 46,
+        high: 23
     },
     green: {
         low: 16,
-        medium:32,
-        high:48
+        medium: 32,
+        high: 48
     }
 };
 
 exports.Launchpad = Launchpad;
 
-exports.connect = function(port, initAnimation) {
+exports.connect = function (port, initAnimation) {
     return new Launchpad(port, initAnimation);
 };
+
